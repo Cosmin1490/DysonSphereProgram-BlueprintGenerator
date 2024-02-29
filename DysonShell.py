c758969d52adb7ac2b8fdf33dcdb4032a0623174
Radu-Cosmin Balan <rbalan@palantir.com>
2024-02-28 18:16:23 -0800

    dyson shell

---
+++ DysonShell.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass
+from typing import Tuple, List
+import numpy as np
+
+@dataclass
+class DysonShell:
+    shellId: int
+    protoId: int
+    randSeed: int
+    color: Tuple[np.int8, np.int8, np.int8, np.int8]
+    nodes: List[int]
+
+    def __init__(self, shellId: int, protoId: int, randSeed: int, color: Tuple[int, int, int, int], nodes: List[int]):
+        self.shellId = shellId
+        self.protoId = protoId
+        self.randSeed = randSeed
+        self.color = (np.int8(color[0]), np.int8(color[1]), np.int8(color[2]), np.int8(color[3]))
+        self.nodes = nodes
+
+    @classmethod
+    def create_with_defaults(cls, shellId: int, nodes: List[int]):
+        default_protoId = 1
+        default_randSeed = 0
+        default_color = (0, 0, 0, 0)
+
+        return cls(shellId, default_protoId, default_randSeed, default_color, nodes)
+
+    def export_as_blueprint(self, w) -> None:
+        w.write(2)
+        w.write(self.shellId)
+        w.write(self.protoId)
+        w.write(self.randSeed)
+        w.write(self.color[0])
+        w.write(self.color[1])
+        w.write(self.color[2])
+        w.write(self.color[3])
+        w.write(len(self.nodes))
+        for node in self.nodes:
+            w.write(node)
--- script.py
+++ script.py
@@ -5,6 +5,8 @@ import base64
 import urllib.parse
 from lib.dspbptk.MD5 import DysonSphereMD5
 from DysonNode import DysonNode
+from DysonFrame import DysonFrame
+from DysonShell import DysonShell
 from BinaryWriter import BinaryWriter
 
 
@@ -22,15 +24,19 @@ def from_blueprint_string(bp_string, validate_hash = True):
 with open("bp.txt") as f:
     from_blueprint_string(f.read())
 
-node = DysonNode.create_with_defaults(1, (0.0, 1.0, 2.0))
-
-
-# with open('bpgen.txt', 'wb') as f:
-#     writer = BinaryWriter(f, hex_output = False)
-#     node.export_as_blueprint(writer)
-
 with open('bpgen.txt', 'w') as f:
+    node = DysonNode.create_with_defaults(1, (0.0, 1.0, 2.0))
     writer = BinaryWriter(f, hex_output = True)
     node.export_as_blueprint(writer)
+    writer.close()
+with open('bpgen2.txt', 'w') as f:
+    node = DysonFrame.create_with_defaults(1, 1, 2)
+    writer = BinaryWriter(f, hex_output = True)
+    node.export_as_blueprint(writer)
+    writer.close()
 
-writer.close()
+with open('bpgen3.txt', 'w') as f:
+    node = DysonShell.create_with_defaults(1, [1, 2, 3])
+    writer = BinaryWriter(f, hex_output = True)
+    node.export_as_blueprint(writer)
+    writer.close()
