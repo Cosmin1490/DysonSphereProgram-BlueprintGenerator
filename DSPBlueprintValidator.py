32c790f4525ad267bad70b7a32d96aad018b3771
name <email>
2024-03-02 09:45:49 -0800

    kdtree

--- DSPBlueprintValidator.py
+++ DSPBlueprintValidator.py
@@ -1,22 +1,21 @@
 import numpy as np
+
+from scipy.spatial import KDTree
+
 from Polyhedron import Polyhedron
 
 class DSPBlueprintValidator:
 
     @staticmethod
-    def euclidean_distance(v1, v2):
-        return np.sqrt(np.sum((np.array(v1) - np.array(v2)) ** 2))
-
-    @staticmethod
     def validate_vertices(polyhedron, min_distance=0.0715, tolerance=1e-9):
         vertices = Polyhedron.project_to_sphere(polyhedron.vertices, 1)
-        for face in polyhedron.faces:
-            for i in range(len(face)):
-                v1 = vertices[face[i]]
-                v2 = vertices[face[(i + 1) % len(face)]]
-                distance = DSPBlueprintValidator.euclidean_distance(v1, v2)
-                if distance - min_distance < tolerance:
-                    return False
+        tree = KDTree(vertices)
+        for i in range(len(vertices)):
+            # Query the KDTree to find the nearest neighbor distance
+            distances, indices = tree.query(vertices[i], 2)  # 2 because the nearest neighbor will be the point itself
+            nearest_neighbor_distance = distances[1]  # 0 index will be the distance to the point itself, which is 0
+            if nearest_neighbor_distance - min_distance < tolerance:
+                return False
         return True
 
     @staticmethod
