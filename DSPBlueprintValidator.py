8ba5e1ce9e482604fdfb10cb805eb9f4e677eda4
Radu-Cosmin Balan <rbalan@palantir.com>
2024-03-19 12:39:36 -0700

    v0 check edges

--- DSPBlueprintValidator.py
+++ DSPBlueprintValidator.py
@@ -11,16 +11,30 @@ class DSPBlueprintValidator:
         vertices = Polyhedron.project_to_sphere(polyhedron.vertices, 1)
         tree = KDTree(vertices)
         for i in range(len(vertices)):
-            # Query the KDTree to find the nearest neighbor distance
-            distances, indices = tree.query(vertices[i], 2)  # 2 because the nearest neighbor will be the point itself
-            nearest_neighbor_distance = distances[1] ** 2  # 0 index will be the distance to the point itself, which is 0
-            if nearest_neighbor_distance < min_distance :
+            distances, indices = tree.query(vertices[i], 2)
+            nearest_neighbor_distance = distances[1] ** 2
+            if nearest_neighbor_distance < min_distance:
                 return False
         return True
 
     @staticmethod
+    def validate_edges(polyhedron, min_distance=0.00275625):
+        vertices = np.array(polyhedron.vertices)
+        for edge in polyhedron.edges:
+            v1, v2 = edge
+            for i, vertex in enumerate(vertices):
+                if i not in edge:
+                    distance_sqr = DSPBlueprintValidator.point_to_segment_sqr(vertices[v1], vertices[v2], vertex)
+                    if distance_sqr < min_distance:
+                        return False
+        return True
+
+    @staticmethod
     def validate_polyhedron(polyhedron, min_distance=0.00511225):
-        return DSPBlueprintValidator.validate_vertices(polyhedron, min_distance)
+        return (
+            DSPBlueprintValidator.validate_vertices(polyhedron, min_distance) and
+            DSPBlueprintValidator.validate_edges(polyhedron, min_distance)
+        )
 
     @staticmethod
     def correct_polyhedron(polyhedron, min_distance=0.00511225):
@@ -32,14 +46,12 @@ class DSPBlueprintValidator:
         vertices = Polyhedron.project_to_sphere(polyhedron.vertices, 1)
         tree = KDTree(vertices)
 
-        # Continue checking and removing invalid vertices until no change
         while True:
             invalid_indices = [i for i, v in enumerate(vertices) if not is_valid_vertex(v, tree)]
 
             if not invalid_indices:
                 break
 
-            # Remove the first invalid vertex and update the tree
             index_to_remove = invalid_indices[0]
             polyhedron.delete_vertex(index_to_remove)
             vertices = np.delete(vertices, index_to_remove, axis=0)
@@ -47,4 +59,13 @@ class DSPBlueprintValidator:
 
         return polyhedron
 
+    @staticmethod
+    def point_to_segment_sqr(begin, end, point):
+        rhs = end - begin
+        sqr_magnitude = np.sum(rhs ** 2)
 
+        if sqr_magnitude < 1e-10:
+            return np.sum((point - begin) ** 2)
+
+        num2 = np.clip(np.dot(point - begin, rhs) / sqr_magnitude, 0, 1)
+        return np.sum(((begin + num2 * rhs) / np.linalg.norm(begin + num2 * rhs) - point) ** 2)
